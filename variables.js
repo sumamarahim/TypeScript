31f74b4fb48f346c4bd5bfd77fcf9a6ede952e3e
Sumama Rahim <69747938+sumamarahim@users.noreply.github.com>
2023-07-17 00:33:29 +0500

    Create variables.js

---
+++ variables.js
@@ -0,0 +1,8 @@
+let firstName = "sumama";
+
+// type annotations on variables
+// when we declare a var using let, const, or var, we can optionally add explicitly specify the type of var
+
+const name1: String = "ali";
+
+console.log(firstName);
