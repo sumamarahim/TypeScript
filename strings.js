4c89bad39ed93bf09fe0f6ff8b0ec946f3d83895
Sumama Rahim <69747938+sumamarahim@users.noreply.github.com>
2023-07-21 16:21:52 +0500

    Add files via upload

---
+++ strings.js
@@ -0,0 +1,17 @@
+// all strings and operations on it
+console.log("Sting and Operations On It!");
+var fName = "Musa";
+var lName = "Khan";
+var national = "PAK";
+var old = 23;
+var height = 5.4;
+var eyeColor = "Brown";
+console.log(fName + " is a " + national + " National" + " He is " + old + " years old and his eye color and height are " + eyeColor + " and " + height + " feet and inches");
+// template string
+//console.log(`${fName} is a ${national} National He is ${old} years old and his eye color and height are ${eyeColor} and ${height} feet and inches`);
+var numb1 = "323";
+var numb2 = 32;
+var numb3 = 3;
+var result = numb1 + numb2;
+console.log(result);
+console.log(numb2 + numb3);
